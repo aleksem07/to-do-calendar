24556c52631c4317a347d305b2754522a234423a
aleksem07 <aleksem07@gmail.com>
2024-07-08 21:56:46 +0300

    feat: add all days in month

--- src/components/calendar-render.tsx
+++ src/components/calendar-render.tsx
@@ -4,14 +4,23 @@ import { MONTH_NAMES } from "../common/month-names";
 
 interface ICalendarRender {
   month: number;
+  year: number;
 }
 
-const CalendarRender: React.FC<ICalendarRender> = ({ month }) => {
+const CalendarRender: React.FC<ICalendarRender> = ({ month, year }) => {
+  const allDays = new Date(year, month + 1, 0).getDate();
+
   return (
     <section className={style.render}>
       <h2 className={style.render_month}>{MONTH_NAMES[month]}</h2>
 
-      <div className={style.render}></div>
+      <ul className={`${style.calendar} ${style.calendar_list}`}>
+        {[...Array(allDays).keys()].map(day => (
+          <li className={style.calendar_item} key={day}>
+            {day + 1}
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
--- src/components/get-calendar.tsx
+++ src/components/get-calendar.tsx
@@ -2,13 +2,13 @@ import GetCalendarData from "./get-calendar-data";
 import CalendarRender from "./calendar-render";
 
 const GetCalendar = () => {
-  const checkedMonth = 10;
+  const checkedMonth = 1;
   const checkedYear = 2024;
 
   return (
     <main>
       <GetCalendarData month={checkedMonth} year={checkedYear} />
-      <CalendarRender month={checkedMonth} />
+      <CalendarRender month={checkedMonth} year={checkedYear} />
     </main>
   );
 };
