e1931579d2e86ca88d7c07ce3e6fae923ab3367b
aleksem07 <aleksem07@gmail.com>
2024-07-08 18:29:47 +0300

    feat: add prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 const App = () => {
   return (
     <>
-    <h1>ToDo Calendar</h1>
+      <h1>ToDo Calendar</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
